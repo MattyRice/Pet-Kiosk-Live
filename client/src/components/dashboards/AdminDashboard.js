5c95512d62fe501ef50fdeb9c7f46b2d04ecbb7b
Matthew Ricio <matthewricio98@gmail.com>
2020-11-04 18:22:27 -0500

    Added Admin Dashboard Buttons

--- client/src/components/dashboards/AdminDashboard.js
+++ client/src/components/dashboards/AdminDashboard.js
@@ -1,17 +1,49 @@
 import React from "react";
+import { MDBBtn, MDBIcon, MDBContainer, MDBRow, MDBCol } from "mdbreact";
 
 const AdminDashboard = () => {
+  // VIEWS
+
+  const showHeader = () => (
+    <div className="container border-bottom mt-5 mx-auto">
+      <div className="row">
+        <i
+          className="fa fa-home fa-2x align-self-center"
+          id="dashboardIcon"
+        ></i>
+        <h1 className="ml-2 display-5 align-self-center">Overview</h1>
+      </div>
+    </div>
+  );
+
+  const showActionBtns = () => (
+    <MDBContainer>
+      <MDBRow className="py-3">
+        <MDBCol>
+          <MDBBtn color="primary">
+            <MDBIcon icon="plus-circle" className="" /> Add Category
+          </MDBBtn>
+        </MDBCol>
+        <MDBCol>
+          <MDBBtn color="default">
+            <MDBIcon icon="plus-circle" className="" /> Add Product
+          </MDBBtn>
+        </MDBCol>
+        <MDBCol>
+          <MDBBtn color="warning">
+            <MDBIcon icon="truck" className="" /> View Orders
+          </MDBBtn>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+
+  //RENDER
+
   return (
     <section>
-      <div className="container border-bottom mt-5 mx-auto">
-        <div className="row">
-          <i
-            className="fa fa-home fa-2x align-self-center"
-            id="dashboardIcon"
-          ></i>
-          <h1 className="ml-2 display-4 align-self-center">Overview</h1>
-        </div>
-      </div>
+      {showHeader()}
+      {showActionBtns()}
     </section>
   );
 };
